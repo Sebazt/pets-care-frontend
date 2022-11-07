a9151c0149026f889eb09f6353ae945ef9425009
Sebazt <juan.londono29@udea.edu.co>
2022-11-07 15:35:55 -0500

    delete library primeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
