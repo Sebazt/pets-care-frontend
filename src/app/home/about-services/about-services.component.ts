10c0d1e66b8f76eb2632ce8b833f6c5962aa5b0e
DiegoMorales <diego.mm09@hotmail.com>
2022-11-11 15:03:56 -0500

    fix

---
+++ src/app/home/about-services/about-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-services',
+  templateUrl: './about-services.component.html',
+  styleUrls: ['./about-services.component.scss']
+})
+export class AboutServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
