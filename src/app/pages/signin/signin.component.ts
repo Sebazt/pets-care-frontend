e460888474692a3cb315257993d91bbc4ef8adb0
Sebazt <juan.londono29@udea.edu.co>
2022-11-11 23:49:38 -0500

    :lipstick: add structure and css about sign in form

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public async goToHome(): Promise<void> {
+    await this.router.navigate(['/']);
+  }
+
+  public async goToSignIn():Promise<void>{
+    await this.router.navigate(['/signup'])
   }
 
 }
