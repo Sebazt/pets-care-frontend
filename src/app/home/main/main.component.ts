e9246fdf4f5b6ea867f1132592d05bf95cd39857
Sebazt <juan.londono29@udea.edu.co>
2022-11-14 17:26:51 -0500

    :rocket: ad functions in Home component

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public goToService():void{
+    this.router.navigate(['/aboutServices']);
+  }
+
+  public goToPetDayCare():void{
+    this.router.navigate(['/petdaycare']);
+  }
+
+  public goToPetWalker():void{
+    this.router.navigate(['/petwalker']);
+  }
+
+  public goToPetGrooming():void{
+    this.router.navigate(['/petgrooming']);
   }
 
 }
